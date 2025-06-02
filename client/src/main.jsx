920777b81f1e38ccb4900936fc48ce8b69aba214
ghonilcloud <onealwaseem@gmail.com>
2025-06-02 14:30:59 +0700

    deploy cicd pipeline

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.GOOGLE_CLIENT_ID}>
       <AuthProvider>
         <BrowserRouter>
           <App />
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,21 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: mode === "development"
+            ? "http://localhost:3000"
+            : "https://e2425-wads-l4bcg2-server.csbihub.id",
+          changeOrigin: true,
+          secure: mode !== "development",
+          ws: true,
+        },
+      },
+    },
+    build: {
+      outDir: "dist",
+    },
+    plugins: [react()],
   }
 })
