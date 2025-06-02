3a8ae48daf75f3f0e3408e58bdfc16b2c92d13ba
ghonilcloud <onealwaseem@gmail.com>
2025-06-02 20:33:29 +0700

    syntax fixed

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-}
+})
 
 // Initialize Passport and CORS
 app.use(passport.initialize());
