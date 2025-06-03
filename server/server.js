321e9e16dd4ebf817215e755ec03ae5f3a17acd5
ghonilcloud <onealwaseem@gmail.com>
2025-06-04 01:58:36 +0700

    server.js removed passport

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const passport = require('passport');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
