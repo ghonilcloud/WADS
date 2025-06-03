f8088e07d9cb98f15f4c80b1406228e878e56d3a
ghonilcloud <onealwaseem@gmail.com>
2025-06-04 00:33:32 +0700

    removed everything, added login and signup functionality backend

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
+const cors = require('cors');
+
+const app = express();
+
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: process.env.JWT_SECRET || 'default_secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(cookieParser());
+
+const userRoutes = require('./routes/userRoutes');
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT;
+
+app.use('/api/user', userRoutes);
+
+mongoose.set('strictQuery', true);
+mongoose.connect(CONNECTION_URL)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    })
+    .catch((error) => console.log('Database connection error:', error.message));
